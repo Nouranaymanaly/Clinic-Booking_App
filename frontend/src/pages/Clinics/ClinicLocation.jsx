df3d4316dd617025a89e440ef0aac4f466b90f3b
HabeebaWalid <beewalidd@gmail.com>
2024-09-04 23:55:06 +0300

    Pages Layout

---
+++ frontend/src/pages/Clinics/ClinicLocation.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ClinicLocation = () => {
+    return (
+        <div>
+        <div>
+            <h3 className = 'text-[17px] leading-[30px] font-bold text-headingColor'> Address 
+                <p className = 'text__para text-[#40968c] font-bold text-[12px]'> ABC, CAIRO, EGYPT </p>
+            </h3>
+        </div>
+    </div>
+    )
+};
+
+export default ClinicLocation;
